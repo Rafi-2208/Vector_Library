c6a68827d0762626c400fac52f14e0c2dd147da5
Rafi <rafal.mamenas@gmail.com>
2025-11-29 16:22:48 +0100

    Fixed

--- vector2d.py
+++ vector2d.py
@@ -1,49 +1,81 @@
-
 class Vector2D:
     def __init__(self, arr):
         self.x = arr[0]
         self.y = arr[1]
         if len(arr) != 2:
             raise TypeError("Vector2D can only be of length 2")
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
     def __add__(self, other):
-        return Vector2D([self.x + other.x, self.y + other.y])
-    def __radd__(self, other:float):
+        if isinstance(other, Vector2D):
+            return Vector2D([self.x + other.x, self.y + other.y])
         return Vector2D([self.x + other, self.y + other])
+
+    def __radd__(self, other):
+        if isinstance(other, Vector2D):
+            return Vector2D([self.x + other.x, self.y + other.y])
+        return Vector2D([self.x + other, self.y + other])
+
     def __sub__(self, other):
-        return Vector2D([self.x - other.x, self.y - other.y])
-    def __rsub__(self, other: float):
+        if isinstance(other, Vector2D):
+            return Vector2D([self.x - other.x, self.y - other.y])
         return Vector2D([self.x - other, self.y - other])
+
+    def __rsub__(self, other: float):
+        if isinstance(other, Vector2D):
+            return Vector2D([self.x - other.x, self.y - other.y])
+        return Vector2D([self.x - other, self.y - other])
+
     def __mul__(self, other):
-        return Vector2D([self.x * other.x, self.y * other.y])
-    def __rmul__(self, other: float):
+        if isinstance(other, Vector2D):
+            return Vector2D([self.x * other.x, self.y * other.y])
         return Vector2D([self.x * other, self.y * other])
+
+    def __rmul__(self, other: float):
+        if isinstance(other, Vector2D):
+            return Vector2D([self.x * other.x, self.y * other.y])
+        return Vector2D([self.x * other, self.y * other])
+
     def __truediv__(self, other):
-        return Vector2D([self.x / other.x, self.y / other.y])
-    def __rtruediv__(self, other: float):
+        if isinstance(other, Vector2D):
+            return Vector2D([self.x / other.x, self.y / other.y])
         return Vector2D([self.x / other, self.y / other])
+
+    def __rtruediv__(self, other: float):
+        if isinstance(other, Vector2D):
+            return Vector2D([self.x / other.x, self.y / other.y])
+        return Vector2D([self.x / other, self.y / other])
+
     def __floordiv__(self, other):
-        return Vector2D([self.x // other.x, self.y // other.y])
-    def __rfloordiv__(self, other:float):
+        if isinstance(other, Vector2D):
+            return Vector2D([self.x // other.x, self.y // other.y])
         return Vector2D([self.x // other, self.y // other])
+
+    def __rfloordiv__(self, other: float):
+        if isinstance(other, Vector2D):
+            return Vector2D([self.x // other.x, self.y // other.y])
+        return Vector2D([self.x // other, self.y // other])
+
     def __mod__(self, other):
-        return Vector2D([self.x % other.x, self.y % other.y])
-    def __rmod__(self, other: float):
+        if isinstance(other, Vector2D):
+            return Vector2D([self.x % other.x, self.y % other.y])
         return Vector2D([self.x % other, self.y % other])
-    def __rpow__(self, other : float):
+
+    def __rmod__(self, other: float):
+        if isinstance(other, Vector2D):
+            return Vector2D([self.x % other.x, self.y % other.y])
+        return Vector2D([self.x % other, self.y % other])
+
+    def __rpow__(self, other: float):
+        if isinstance(other, Vector2D):
+            return Vector2D([self.x ** other.x, self.y ** other.y])
         return Vector2D([self.x ** other, self.y ** other])
 
-#-----testing area-----
+
+
+
+# -----testing area-----
 if __name__ == "__main__":
-    import time
-    import numpy as np
-    a = Vector2D([1, 2, 3])
-    a = [4,8]
-    t = time.time()
-    v = Vector2D(a)
-    for i in range(10000):
-        z = v + v
-    print(time.time() - t)
-    v = np.array(a)
-    for i in range(10000):
-        z = v + v
-    print(time.time() - t)
+    pass
