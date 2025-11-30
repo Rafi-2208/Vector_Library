d78a963fadc9b84c59cef232f873e253aaf07acf
Rafi <rafal.mamenas@gmail.com>
2025-11-30 18:38:43 +0100

    0.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rafivector",
-    version="0.4.0",
+    version="0.5.0",
     description="A simple library for vector operations",
     author="Rafi",
     packages=find_packages(),
