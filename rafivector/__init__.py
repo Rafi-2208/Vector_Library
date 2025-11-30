bf2aeba7942cb446d660fcc3a3d1498eb62e92c6
Rafi <rafal.mamenas@gmail.com>
2025-11-30 16:19:00 +0100

    build

--- rafivector/__init__.py
+++ rafivector/__init__.py
@@ -1,2 +1,3 @@
 from .vector2d import Vector2D , dot , cross , angle , project , reflect
-__all__ = ["Vector2D", "dot", "cross", "angle", "project", "reflect"]
+from line2d import Line2D , colision
+__all__ = ["Vector2D", "dot", "cross", "angle", "project", "reflect" , "Line2D" , "colisions"]
---
+++ rafivector/line2d.py
@@ -0,0 +1,27 @@
+from vector2d import *
+
+class Line2D:
+    def __init__(self , p1:'Vector2D' , p2:'Vector2D'):
+        self.p1=p1
+        self.p2=p2
+
+
+def colision(l1 , l2):
+    x1 = l1.p1.x
+    x2 = l1.p2.x
+    y1 = l1.p1.y
+    y2 = l1.p2.y
+    x3 = l2.p1.x
+    x4 = l2.p2.x
+    y3 = l2.p1.y
+    y4 = l2.p2.y
+    den = (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4)
+    if den == 0:
+        return None
+    px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / den
+    py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / den
+    if min(x1, x2) <= px <= max(x1, x2) and min(y1, y2) <= py <= max(y1, y2) and min(x3, x4) <= px <= max(x3, x4) and min(y3, y4) <= py <= max(y3, y4):
+        return px, py
+    return None
+
+
