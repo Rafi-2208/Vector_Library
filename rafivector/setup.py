a69afb699a1a9da4090a16c34a649de7560788b9
Rafi <rafal.mamenas@gmail.com>
2025-11-30 13:01:56 +0100

    changed structure for libray

---
+++ rafivector/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rafivector",
+    version="0.1.0",
+    description="A simple library for vector operations",
+    author="Rafi",
+    packages=find_packages(),
+    install_requires=[],  # dependencies if any
+)
