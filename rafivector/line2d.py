b4df308a4e7c2e94162690c4a49824f36c068d52
Rafi <rafal.mamenas@gmail.com>
2025-11-30 18:46:11 +0100

    0.7.0

--- rafivector/line2d.py
+++ rafivector/line2d.py
@@ -34,7 +34,7 @@ def collision(l1, l2):
     px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / den
     py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / den
     if min(x1, x2) <= px <= max(x1, x2) and min(y1, y2) <= py <= max(y1, y2) and min(x3, x4) <= px <= max(x3, x4) and min(y3, y4) <= py <= max(y3, y4):
-        return px, py
+        return round(px,10), round(py,10)
     return None
 
 
